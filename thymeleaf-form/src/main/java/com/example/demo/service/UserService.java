77bf4155dc4c3cb83c5b16a4ee5d5e777cf7ce45
137ryo <ryo167prog@gmail.com>
2022-01-23 12:34:59 +0900

    削除/編集機能の追加

--- thymeleaf-form/src/main/java/com/example/demo/service/UserService.java
+++ thymeleaf-form/src/main/java/com/example/demo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.UserRequest;
+import com.example.demo.dto.UserUpdateRequest;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
@@ -43,5 +44,34 @@ public class UserService {
     user.setCreateDate(now);
     user.setUpdateDate(now);
     userRepository.save(user);
+  }
+
+  /**
+   * ユーザー情報 主キー検索
+   * @return 検索結果
+   */
+  public User findById(Long id) {
+    return userRepository.findById(id).get();
+  }
+  /**
+   * ユーザー情報 更新
+   * @param user ユーザー情報
+   */
+  public void update(UserUpdateRequest userUpdateRequest) {
+    User user = findById(userUpdateRequest.getId());
+    user.setAddress(userUpdateRequest.getAddress());
+    user.setName(userUpdateRequest.getName());
+    user.setPhone(userUpdateRequest.getPhone());
+    user.setUpdateDate(new Date());
+    userRepository.save(user);
+  }
+
+  /**
+   * ユーザー情報 物理削除
+   * @param id ユーザーID
+   */
+  public void delete(Long id) {
+    User user = findById(id);
+    userRepository.delete(user);
   }
 }
